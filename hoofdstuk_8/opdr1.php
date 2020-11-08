5008561a8cb21c1ca107d14f34ec6fb64dc3a9db
Bo-Eamonn <bo.eamonn@gmail.com>
2020-11-08 16:12:08 +0100

    Bo | did opdr1

---
+++ hoofdstuk_8/opdr1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Opdrachten Bo PhP7</title>
+
+    <link rel="stylesheet" href="/php7_assignments/static/styles/style.css">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.js"
+        integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="/php7_assignments/static/js/template.js"></script>
+</head>
+
+<body>
+    <div id="header"></div><br><br><br><br>
+
+    <div id="container">
+        <p>
+            In paragraf 8.3 wordt uitvoerig besproken hoe je met het programma PHPMyAdmin een database kunt aanmaken. <br>
+            Voer alle stappen uit die in paragraaf 8.3 genoemd zijn.
+            <hr>
+            Zie hier onder voor de output van de laatste stap van paragraaf 8.3
+        </p>
+        <img src="/php7_assignments/static/img/hoofdstuk8opdr1final.jpeg" alt="Final assignments output" title="Resultaat van de laaste opdracht als check">
+    </div>
+
+    <div id="footer"></div>
+</body>
+
+</html>
